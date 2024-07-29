1f3682d5c2108e61233a057014a4c58d6f3aac70
Steven Goldberg <rotoclone@gmail.com>
2024-07-29 02:59:27 -0500

    Fix name and remove more unused audio

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
-use bevy_jam_05::AppPlugin;
+use looprunner::AppPlugin;
 
 fn main() -> AppExit {
     App::new().add_plugins(AppPlugin).run()
